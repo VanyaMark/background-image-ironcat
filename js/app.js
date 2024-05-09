c4999baee2621b86ab038ff3c3721bbf37a87af8
VanyaMark <markova.vanya@gmail.com>
2024-05-09 19:46:06 +0200

    done

--- js/app.js
+++ js/app.js
@@ -10,3 +10,37 @@ const pictures = [
  * ¿Qué variable de estado vamos a necesitar?
  * ¿Cómo se establece la imagen de fondo de un elemento mediante un estilo en línea?
  */
+
+const btnLeft = document.querySelector('.btn-left');
+const btnRight = document.querySelector('.btn-right');
+const slider = document.querySelector('.img-container')
+
+let index = 0;
+
+// document.querySelector('.img-container').style.backgroundImage = `url(${pictures[index]})`;
+
+slider.style.backgroundImage = `url(${pictures[index]})`;
+
+const handleClickBtnLeft = () => {
+  index--;
+  if (index >= 0 && index < 4) {
+    console.log(index);
+    slider.style.backgroundImage = `url(${pictures[index]})`;
+  } else {
+    index = 4;
+    slider.style.backgroundImage = `url(${pictures[index]})`;
+  }
+}
+const handleClickBtnRight = () => {
+  index++;
+  if (index >= 0 && index <= 4) {
+    console.log(index);
+    slider.style.backgroundImage = `url(${pictures[index]})`;
+  } else {
+    index = 0;
+    slider.style.backgroundImage = `url(${pictures[index]})`;
+  }
+}
+
+btnLeft.addEventListener('click', handleClickBtnLeft);
+btnRight.addEventListener('click', handleClickBtnRight);
